31cb9efddf0251d9792fa6419de6df8b85dc8ace
sarfa <sarfa@LAPTOP-9S85VORI>
2023-04-09 09:29:01 +0530

    user storey numcer 130 changes done in xyz TC related to PO correction

---
+++ demo/src/first/basic_2.java
@@ -0,0 +1,22 @@
+package first;
+
+public class basic_2 {
+	public class basic_1 {
+		/*non static method  declare in same class and call in different class*/
+		
+		
+		public void Demo() {  
+			/*user define non static method*/
+			 System.out.println("Demo method is running");
+		  }
+		
+		
+		public static void Test() {   
+			/*user define static method*/
+			System.out.println("Test  method is running");
+		}
+		
+
+	}
+
+}
